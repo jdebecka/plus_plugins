0d53e208fc66eaf952e1a3387b87b02c38da65da
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2021-12-14 18:33:56 +0100

    refactor: remove image support

--- packages/share_plus/share_plus/ios/Classes/FLTSharePlusPlugin.m
+++ packages/share_plus/share_plus/ios/Classes/FLTSharePlusPlugin.m
@@ -234,18 +234,8 @@ static UIViewController *TopViewControllerForViewController(UIViewController *vi
     NSString *pathExtension = [path pathExtension];
     NSString *mimeType = mimeTypes[i];
 
-    if ([pathExtension.lowercaseString isEqualToString:@"jpg"] ||
-        [pathExtension.lowercaseString isEqualToString:@"jpeg"] ||
-        [pathExtension.lowercaseString isEqualToString:@"png"] ||
-        [mimeType.lowercaseString isEqualToString:@"image/jpg"] ||
-        [mimeType.lowercaseString isEqualToString:@"image/jpeg"] ||
-        [mimeType.lowercaseString isEqualToString:@"image/png"]) {
-      NSData *image = [NSData dataWithContentsOfURL:path];
-      [items addObject:image];
-    } else {
-      NSURL *fileUrl = [NSURL fileURLWithPath:path];
-      [items addObject:fileUrl];
-    }
+    NSURL *fileUrl = [NSURL fileURLWithPath:path];
+    [items addObject:fileUrl];
   }
 
   [self share:items withController:controller atSource:origin];
